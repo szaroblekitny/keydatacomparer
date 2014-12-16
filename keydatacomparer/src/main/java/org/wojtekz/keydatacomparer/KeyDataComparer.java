b85389598efd93fd147ec8ca98c8e0cfa61ce06d
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-12-16 19:54:07 +0100

    Sonar issues:
    -- removed System.exit
    -- private constructor
    -- logg -> LOGG
    -- main method splitted
    -- minor others

--- keydatacomparer/src/main/java/org/wojtekz/keydatacomparer/KeyDataComparer.java
+++ keydatacomparer/src/main/java/org/wojtekz/keydatacomparer/KeyDataComparer.java
@@ -17,7 +17,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+
 import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
@@ -36,24 +38,37 @@ import org.xml.sax.SAXException;
  */
 public class KeyDataComparer {
 
-    private static Logger logg = Logger.getLogger(KeyDataComparer.class.getName());
+    private final static Logger LOGG = Logger.getLogger(KeyDataComparer.class.getName());
     private static BazaDanych bazaWzorcowa;
     private static BazaDanych bazaPorownywana;
-    private static File outputFile = new File("output.txt");
+    private static File outputFile;
+    
+    // private constructor hides instatiation of this class
+    private KeyDataComparer() {
+    }
 
     /**
-     * @param args the command line arguments
+     * Main method of application. Reads configuration file and makes comparison.
+     * 
+     * @param args the command line arguments: config file name and optional output file name
      */
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        logg.info("Key data comparer starts");
+        LOGG.info("Key data comparer starts");
 
-        if (args.length != 1) {
-            logg.fatal("Argumentem jest nazwa pliku z tabelami do porównania");
-            System.exit(1);
+        if (args.length != 1 && args.length != 2) {
+            LOGG.fatal("Argumentem jest nazwa pliku z tabelami do porównania");
+            throw new RuntimeException();
         }
+        
+        if (args.length == 2) {
+        	outputFile = new File(args[1]);
+        } else {
+        	outputFile = new File("output.txt");
+        }
+        
         for (int ii = 0; ii < args.length; ii++) {
-            logg.debug("arg[" + ii + "]: " + args[ii]);
+            LOGG.debug("arg[" + ii + "]: " + args[ii]);
         }
 
         try {
@@ -70,51 +85,61 @@ public class KeyDataComparer {
                     obspli.getCompusername(),
                     obspli.getCompuserpassword());
             
-            try (FileWriter zapisywacz = new FileWriter(outputFile)) {
-                zapisywacz.write("Porównanie baz \n");
-                zapisywacz.write("Baza wzorcowa: " + bazaWzorcowa.getDatabase() +
-                        "/" + bazaWzorcowa.getUsername() + "\n");
-                zapisywacz.write("Baza porównywana: " + bazaPorownywana.getDatabase() + 
-                        "/" + bazaPorownywana.getUsername() + "\n");
-                // zapisywacz.close();
-                
-                Connection bwzorConn = bazaWzorcowa.databaseConnection();
-                Connection bporowConn = bazaPorownywana.databaseConnection();
-
-                if (logg.isTraceEnabled()) {
-                    logg.trace("Mamy popr. połączenie wzor" + bwzorConn.isValid(10));
-                    logg.trace("Mamy popr. połączenie por" + bporowConn.isValid(11));
-                }
-                Porownywacz por = new Porownywacz(zapisywacz);
-                por.porownuj(bazaWzorcowa, bazaPorownywana, obspli.getNazwyTabel());
-                
-                zapisywacz.write("======================================================\n");
-                zapisywacz.write("**** KONIEC PORÓWNANIA ****\n");
-                zapisywacz.close();
-
-                if (bwzorConn != null) {
-                    bwzorConn.close();
-                    logg.debug("Połączenie bazy wzorcowej zamknięte");
-                }
-                
-                if (bporowConn != null) {
-                    bporowConn.close();
-                    logg.debug("Połączenie bazy porównywanej zamknięte");
-                }
-            }
+            porownajIZapisz(obspli);
             
         } catch (SQLException ex) {
-            logg.error("SQLException !! ", ex);
+            LOGG.error("SQLException !! ", ex);
         } catch (IOException ex) {
-            logg.error("IOException: " + ex.getMessage());
+            LOGG.error("IOException: " + ex.getMessage(), ex);
         } catch (SAXException ex) {
-            logg.error("SAXException: " + ex.getMessage());
+            LOGG.error("SAXException: " + ex.getMessage(), ex);
         } catch (ParserConfigurationException ex) {
-            logg.error("ParserConfigurationException: " + ex.getMessage());
+            LOGG.error("ParserConfigurationException: " + ex.getMessage(), ex);
         } catch (Exception ex) {
-            logg.error("Błędny błąd", ex);
+            LOGG.error("Błędny błąd", ex);
         } finally {
-            logg.info("Key data comparer ends");
+            LOGG.info("Key data comparer ends");
         }
+    }
+    
+    /**
+     * Makes comparison and writes output.
+     * 
+     * @param obslugaPlikow reader and parser for configuration XML file
+     * @throws IOException
+     * @throws SQLException
+     */
+    private static void porownajIZapisz(ObsluzPliki obslugaPlikow) throws IOException, SQLException {
+    	FileWriter zapisywacz = new FileWriter(outputFile); 
+        zapisywacz.write("Porównanie baz \n");
+        zapisywacz.write("Baza wzorcowa: " + bazaWzorcowa.getDatabase() +
+                    "/" + bazaWzorcowa.getUsername() + "\n");
+        zapisywacz.write("Baza porównywana: " + bazaPorownywana.getDatabase() + 
+                    "/" + bazaPorownywana.getUsername() + "\n");
+            
+        Connection bwzorConn = bazaWzorcowa.databaseConnection();
+        Connection bporowConn = bazaPorownywana.databaseConnection();
+
+        if (LOGG.isTraceEnabled()) {
+            LOGG.trace("Mamy popr. połączenie wzor" + bwzorConn.isValid(10));
+            LOGG.trace("Mamy popr. połączenie por" + bporowConn.isValid(11));
+        }
+        Porownywacz por = new Porownywacz(zapisywacz);
+        por.porownuj(bazaWzorcowa, bazaPorownywana, obslugaPlikow.getNazwyTabel());
+            
+        zapisywacz.write("======================================================\n");
+        zapisywacz.write("**** KONIEC PORÓWNANIA ****\n");
+        zapisywacz.close();
+
+        if (bwzorConn != null) {
+            bwzorConn.close();
+            LOGG.debug("Połączenie bazy wzorcowej zamknięte");
+        }
+            
+        if (bporowConn != null) {
+            bporowConn.close();
+            LOGG.debug("Połączenie bazy porównywanej zamknięte");
+        }
+        
     }
 }
